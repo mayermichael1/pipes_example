ecaa7a450f503b5dd5bbc8ab15ce881e3ad831e4
mayermichael1 <michael.mayer2105@gmail.com>
2024-06-18 21:47:50 +0200

    cap runs forever and turns every letter to upper

--- cap.cpp
+++ cap.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+#include <ctype.h>
+#include <unistd.h>
 
 int main(void){
-    std::cout << "Hello pipe_example" << std::endl;
+    
+    char buffer = 0;
+    while(true){
+        read(STDIN_FILENO, &buffer, 1);
+        buffer = toupper(buffer);
+        write(STDOUT_FILENO, &buffer, 1);
+    }
+
     return 0;
 }
